ad693d1e77053af2a54a8ce1bee6cdca3cd38050
hajdam <hajdam@users.sf.net>
2020-10-02 23:35:56 +0200

    Lazy day

--- about/pages/goals.php
+++ about/pages/goals.php
@@ -1,77 +1,73 @@
 <div id="content">
-<h1 id="motivation">Goals</h1>
+<h1 id="goals">Goals</h1>
 
-<div class="level1">
+<p>The primary goal of this project is to create a communication protocol / data format with the following characteristics, ordered by priority:</p>
 
-<p>
-<a href="../?video" class="wikilink1">Video presentation</a> about project is recommended for the first time visitors.
-</p>
-
-<p>
-The primary goal of this project is to create communication protocol/data format with the following characteristics:
-</p>
 <ul>
-<li class="level1"><div class="li"> <strong>Universal</strong> - Suitable for any type of data, persistent storage or streaming purposes</div>
-</li>
-<li class="level1"><div class="li"> <strong>Independent</strong> - Not tightly linked to particular spoken language or infrastructure/programming language</div>
-</li>
-<li class="level1"><div class="li"> <strong>Flexible</strong> - Support for data transformations, compatibility and extensibility</div>
-</li>
-<li class="level1"><div class="li"> <strong>Effective</strong> - Data compactness and compression support for plain binary and structured data</div>
-</li>
+<li><strong>Universal</strong> - Capable of representation of any type of data, suitable for wide use including streaming, long-term storage and parallel accessing</li>
+<li><strong>Independent</strong> - Not tightly linked to a particular spoken language, product, company, processing architecture or programming language</li>
+<li><strong>Declarative</strong> - Self sufficient for data type definition and with the ability to build data types by combining existing ones</li>
+<li><strong>Normative</strong> - Providing reference form for data representation</li>
+<li><strong>Flexible</strong> - Support for data transformations, compatibility and extensibility handling</li>
+<li><strong>Efficient</strong> - Effective data compacting / compression support for plain binary and structured data</li>
 </ul>
 
 <p>
-<a href="?motivation" class="wikilink1">More details about what is project trying to achieve.</a>
+<a href="?motivation">More details about what is project trying to achieve</a>
 </p>
 
-</div>
-
-<h2 class="sectionedit2" id="protocol_design">Protocol Design</h2>
-<div class="level2">
+<h2 id="protocol_design">Protocol Design</h2>
+<div>
 
 <p>
 Protocol design should cover following areas:
 </p>
 <ul>
-<li class="level1"><div class="li"> <strong>Data structure</strong> - How to store/stream data</div>
+<li><div><strong>Data structure</strong> - How to store/stream data</div>
 </li>
-<li class="level1"><div class="li"> <strong>Data types</strong> - Declaration of data types and build-in types</div>
+<li><div><strong>Data types</strong> - Declaration of data types and build-in types</div>
 </li>
-<li class="level1"><div class="li"> <strong>Data transformations</strong> - Support for data transformations, like compression, encryption and so on</div>
+<li><div><strong>Data transformations</strong> - Support for data transformations, like compression, encryption and so on</div>
 </li>
 </ul>
 
 <p>
-<a href="../doc/?protocol" class="wikilink1">More details about protocol&#039;s design.</a>
+<a href="../doc/?protocol">More details about protocol&#039;s design</a>
 </p>
 
 </div>
 
-<h2 class="sectionedit3" id="prototype_implementation">Prototype Implementation</h2>
+<h2 id="prototype_implementation">Prototype Implementation</h2>
 <div class="level2">
 
 <p>
 Free and open source prototype implementation in Java is currently underway. It should provide following outputs:
 </p>
 <ul>
-<li class="level1"><div class="li"> <strong>Basic library</strong> - Library providing support for handling data encoded in the protocol itself</div>
+<li><div><strong>Basic library</strong> - Library providing support for handling data encoded in the protocol itself</div>
 </li>
-<li class="level1"><div class="li"> <strong>Catalog of types</strong> - Catalog service providing access to declaration of types</div>
+<li><div><strong>Catalog of types</strong> - Catalog service providing access to declaration of types</div>
 </li>
-<li class="level1"><div class="li"> <strong>Framework</strong> - Local service providing various functions for applications</div>
+<li><div><strong>Framework</strong> - Local service providing various functions for applications</div>
 </li>
-<li class="level1"><div class="li"> <strong>Editor tool</strong> - Basic tool for viewing and edition</div>
+<li><div><strong>Editor tool</strong> - Basic tool for viewing and edition</div>
 </li>
-<li class="level1"><div class="li"> <strong>Sample applications and files</strong> - Applications and files for demonstration purposes</div>
+<li><div><strong>Sample applications and files</strong> - Applications and files for demonstration purposes</div>
 </li>
 </ul>
 
 <p>
-<a href="../doc/?implementation" class="wikilink1">More details about prototype implementation.</a>
+<a href="../doc/?implementation">More details about prototype implementation</a>
 </p>
 
-</div>
+<h2 id="more">More</h2>
+
+<p>You can learn more about this project by reading following sections:</p>
+
+<ul>
+<li><a href="?use_cases">Use Cases</a></li>
+<li><a href="?project_scope">Scope</a></li>
+</ul>
 
 </div>
 </body>
