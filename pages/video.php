ca5ccfd3109b8ee806102bd0783caf1083b5df77
hajdam <hajdam@users.sf.net>
2021-02-17 23:44:00 +0100

    Introduction video 0.2.1 released

--- pages/video.php
+++ pages/video.php
@@ -1,13 +1,19 @@
 <div id="content">
 <h1>Videos About This Project</h1>
 
-<div style="float: left; padding: 0 10px 0 10px;"><p>Basic Overview:</p>
+<div style="padding: 0 10px 0 10px;"><p>XBUP Overview:</p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/pp1hwHE5kaE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<p><em>Get sources of this presentation <a href="https://drive.google.com/file/d/1TKH4AyjSJvLGt-MntMLU9J1kRLsb63Y8/view?usp=sharing">[PDF]</a> <a href="https://drive.google.com/file/d/10oSlNxGLH0sp9dxbFSasaPvASZCz7GKg/view?usp=sharing">[ODP]</a>.</em></p></div>
+
+<h2>Older videos</h2>
+
+<div style="padding: 0 10px 0 10px;"><p>Basic Overview:</p>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/t1oZ163hIdA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <p><em>Get sources of this presentation <a href="http://prdownloads.sourceforge.net/xbup/xbup_overview-wr17.pdf?download">[PDF]</a> <a href="http://prdownloads.sourceforge.net/xbup/xbup_overview-wr17.src.odp?download">[ODP]</a>.</em></p></div>
-<div style="float: left; padding: 0 10px 0 10px;"><p>Advantages of the Unified Structure:</p>
+<div style="padding: 0 10px 0 10px;"><p>Advantages of the Unified Structure:</p>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/fbUq7HmdpKs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <p><em>Get sources of this presentation <a href="http://prdownloads.sourceforge.net/xbup/xbup_unified-wr17.pdf?download">[PDF]</a> <a href="http://prdownloads.sourceforge.net/xbup/xbup_unified-wr17.src.odp?download">[ODP]</a>.</em></p></div>
-<div style="float: left; padding: 0 10px 0 10px;"><p>Architecture of Service:</p>
+<div style="padding: 0 10px 0 10px;"><p>Architecture of Service:</p>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/BME7Z1jjkx4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <p><em>Get sources of this presentation <a href="http://prdownloads.sourceforge.net/xbup/xbup_architecture-wr20.pdf?download">[PDF]</a> <a href="http://prdownloads.sourceforge.net/xbup/xbup_architecture-wr20.src.odp?download">[ODP]</a>.</em></p></div>
 
