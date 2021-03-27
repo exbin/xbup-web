68a6787a6f7581b47b049628b446f16da799d478
hajdam <hajdam@users.sf.net>
2021-03-27 22:45:43 +0100

    Lazy day

--- doc/pages/specification/rfc.php
+++ doc/pages/specification/rfc.php
@@ -1,7 +1,8 @@
 <div id="content">
 <h1>Specification RFC</h1> 
-<p>RFC document is under development in <a class="externurl" href="https://github.com/exbin/xbup-doc/tree/master/rfc">github.com/exbin/xbup-doc/tree/master/rfc</a>.</p>
-<p>Read <a href="rfc/draft-ietf-exbin-xbup-core-00.html">current html build</a> or <a href="rfc/draft-ietf-exbin-xbup-core-00.txt">current text build</a></p>
+<p>RFC document is under development.</p>
+<p>Read current build <a class="urlextern" href="rfc/draft-ietf-exbin-xbup-core-00.html">HTML</a> or <a class="urlextern" href="rfc/draft-ietf-exbin-xbup-core-00.txt">TEXT</a>.</p>
+<p>XML source <a class="urlextern" href="https://github.com/exbin/xbup-doc/tree/master/rfc">github.com/exbin/xbup-doc/tree/master/rfc</a>.</p>
 
 </div>
 </body>
